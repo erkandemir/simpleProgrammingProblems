79d6931ce3f85236a2b53ba222c588c84100923a
Erkan Demir <erkandemir86@gmail.com>
2013-12-16 00:29:46 +0200

    modified

--- choice.rb
+++ choice.rb
@@ -9,16 +9,10 @@ number = gets.to_i
 puts "1 to sum, 2 to product"
 choice = gets.to_i
 
-case choice
-when 1	
-	total = 0
-	1.upto(number.to_i){|num| total += num} 
-	puts total
-when 2
-	mult = 1 
-	1.upto(number.to_i){|num| mult = mult * num}
-	puts mult
-end
+
+puts (1..number).reduce({1 => :+, 2 => :*}[choice])
+
+
 
 	
 
